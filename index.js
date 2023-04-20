72b277ad76ef230e31ecdae42e72e302e5506fc7
Shania Lopez <slopez@fandm.edu>
2023-04-19 20:43:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCase(str) {
+  return str.split(' ').map(function(word){
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
+function titleCased() {
+  return tutorials.map(tutorial => titleCase(tutorial))
 }
